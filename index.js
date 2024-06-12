1fc3e754aee554e9c528f1f85cdd530766f2c02f
Yahya Mohamed <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-06-12 17:50:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+let currentUser = 'Yahya'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, '
+const upperCaseCurrentUser = currentUser.toUpperCase()
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting = `Welcome, ${firstInitial}!`
